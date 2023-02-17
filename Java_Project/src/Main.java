87976ee6b170968223c0666872c5b451a52714e1
Mochalka <bdauren0408@gmail.com>
2023-02-17 19:25:44 +0600

    new

--- Java_Project/src/Main.java
+++ Java_Project/src/Main.java
@@ -74,6 +74,82 @@
 
 
 
+//import java.sql.*;
+//import java.util.Scanner;
+//
+//public class Main {
+//    private static final String DB_USERNAME = "postgres";
+//    private static final String DB_PASSWORD = "0000";
+//    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+//    Connection connection;
+//
+//    public Main() throws SQLException {
+//        connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+//    }
+//
+//    public static void main(String[] args) {
+//        try {
+//            Main main = new Main();
+//            main.run();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public void run() throws SQLException {
+//        Scanner scanner = new Scanner(System.in);
+//        while (true) {
+//            System.out.println(" ");
+//            System.out.println("1. Показать список всех аккаунтов");
+//            System.out.println("2. Создать");
+//            System.out.println("3. Выйти");
+//            System.out.println(" ");
+//
+//            int command = scanner.nextInt();
+//
+//            switch (command) {
+//                case 1:
+//                    Statement st = connection.createStatement();
+//                    String DB_ACCOUNT = "select * from accounts order by Accounts";
+//                    ResultSet result = st.executeQuery(DB_ACCOUNT);
+//
+//                    System.out.println(" ");
+//
+//                    while (result.next()) {
+//                        System.out.println(result.getString("Account") + "   |   " + result.getString("Mail") + "   |   " + result.getString("Login") + "   |   " + result.getString("Password"));
+//                    }
+//                    System.out.println(" ");
+//                    break;
+//
+//                case 2:
+//                    // Take input from user for site, mail, login, and password
+//                    UserInput userInput = new UserInput(scanner);
+//                    Account newAccount = userInput.getAccountInfo();
+//
+//                    // Insert the user inputs into the database
+//                    String sql = "INSERT INTO accounts(account, mail, login, password) VALUES (?, ?, ?, ?)";
+//                    PreparedStatement statement = connection.prepareStatement(sql);
+//                    statement.setString(1, newAccount.getAccount());
+//                    statement.setString(2, newAccount.getMail());
+//                    statement.setString(3, newAccount.getLogin());
+//                    statement.setString(4, newAccount.getPassword());
+//                    int rowsInserted = statement.executeUpdate();
+//                    if (rowsInserted > 0) {
+//                        System.out.println("A new user data has been inserted successfully.");
+//                    }
+//                    break;
+//
+//                case 3:
+//                    System.exit(0);
+//
+//                default:
+//                    System.err.println("Команда не распознана");
+//            }
+//        }
+//    }
+//}
+
+
 import java.sql.*;
 import java.util.Scanner;
 
@@ -123,16 +199,18 @@ public class Main {
 
                 case 2:
                     // Take input from user for site, mail, login, and password
-                    UserInput userInput = new UserInput(scanner);
-                    Account newAccount = userInput.getAccountInfo();
+                    String account = getInput("Account");
+                    String mail = getInput("Mail");
+                    String login = getInput("Login");
+                    String password = getInput("Password");
 
                     // Insert the user inputs into the database
-                    String sql = "INSERT INTO accounts(account, mail, login, password) VALUES (?, ?, ?, ?)";
+                    String sql = "INSERT INTO accounts (account, mail, login, password) VALUES (?, ?, ?, ?)";
                     PreparedStatement statement = connection.prepareStatement(sql);
-                    statement.setString(1, newAccount.getAccount());
-                    statement.setString(2, newAccount.getMail());
-                    statement.setString(3, newAccount.getLogin());
-                    statement.setString(4, newAccount.getPassword());
+                    statement.setString(1, account);
+                    statement.setString(2, mail);
+                    statement.setString(3, login);
+                    statement.setString(4, password);
                     int rowsInserted = statement.executeUpdate();
                     if (rowsInserted > 0) {
                         System.out.println("A new user data has been inserted successfully.");
@@ -146,5 +224,12 @@ public class Main {
                     System.err.println("Команда не распознана");
             }
         }
+    }
+
+    // Method to get input from user
+    public static String getInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(prompt + ": ");
+        return scanner.nextLine();
     }
 }
